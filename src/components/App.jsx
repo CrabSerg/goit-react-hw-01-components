91342902d0d5b8d37cf70af1732bebbc0998075a
Serg <serg212007@gmail.com>
2023-01-02 09:10:50 +0200

    push

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import { Profile } from './Profile/Profile';
+import user from '../json/user.json';
+import { Statistics } from './Statistics/Statistics';
+import statistics from '../json/statistics.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../json/friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../json/transactions.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        padding: 50,
+        height: 'auto',
         display: 'flex',
+        flexDirection: 'column',
+        gap: 100,
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 24,
+        color: '#010101',
+        backgroundColor: 'lightgray',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" statistics={statistics} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
